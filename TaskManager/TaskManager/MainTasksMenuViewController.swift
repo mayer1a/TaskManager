002336eedc3410836d73f892e6489034561be446
Artem V. Mayer <mayer1art@gmail.com>
2022-12-22 17:10:00 +0700

    init project

---
+++ TaskManager/TaskManager/MainTasksMenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTasksMenuViewController.swift
+//  TaskManager
+//
+//  Created by Artem Mayer on 22.12.2022.
+//
+
+import UIKit
+
+final class MainTasksMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
